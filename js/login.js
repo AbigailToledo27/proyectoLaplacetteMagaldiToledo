ce41b92ee312f0819b16f55eca7892f6271cdb5e
ylaplacette-code <ylaplacette@udesa.edu.ar>
2025-11-15 19:40:47 -0300

    loginjs y productjs ya casi están

---
+++ js/login.js
@@ -0,0 +1,25 @@
+let formulario = document.querySelector(".formlogin")
+let email = document.querySelector("#email")
+let pass = document.querySelector(".passlogin")
+
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault()
+    
+    if (email.value == ""){
+        let erroremail = document.querySelector(".email")
+        erroremail.innerText = "El campo es obligatorio, no puede estar vacío"
+        erroremail.style.display = "block"
+        return erroremail
+    }else if (pass.value == ""){
+        let errorpass = document.querySelector(".contraseña")
+        errorpass.innerText = "El campo es obligatorio, no puede estar vacío"
+        errorpass.style.display = "block"
+        return errorpass
+    } else if (pass.value.length < 6){
+        let errorcarac = document.querySelector(".caracteres")
+        errorcarac.innerText = "La contraseña debe tener al menos 6 caracteres"
+        errorcarac.style.display = "block"
+    } else {
+         this.submit()
+    }
+})
--- js/product.js
+++ js/product.js
@@ -9,36 +9,40 @@ fetch(`https://dummyjson.com/products/${id}`)
     })
     .then(function(info){
         let productoInfo = info
-        console.log(data);
+        console.log(productoInfo); // Para ver la info de la api, después hay q borrarlo
         
-        let producto = document.querySelector(".detalleproducto")
+        // Linea 22 --> Quiero agregarle un for para q todos los tags tengan su #, si no queda feito
+        
+        let producto = document.querySelector(".detalleproducto") 
 
-        let carga = ` <div class="divsproducto">
-        <img src=${info.images[0]} alt="foto detalle de producto" class="fotoproducto">
-        <h3><a href="./category.html" class="category">Categoría: </a></h3>
-        <h2 class="precioprod">Precio: </h2>
-        <p class="tagsproducto"></p>
-    </div>
-    <div class="divsproducto">
-        <div>
-        <h1 class="nombreproducto"></h1>
-        <h2 class="titulodescripción">Descripción:</h2>
-        <p class="descripción"></p>
-        <p class="stockproducto">Stock: </p>
-    </div> 
-    <div class="reviwsproducto">
-        <h3 class="nombre reviews">Reviews de Usuarios:</h3>
-        <p class="usuario1 nombre"></p> 
-        <p class="fecha1"></p>
-        <p class="opinion1"></p>
-        <p class="rating1">Rating: </p>
-        <p class="usuario2 nombre"></p>
-        <p class="fecha2"></p>
-        <p class="opinion2"></p>
-        <p class="rating2">Rating: </p>
-    </div>
-    </div>`
-    producto.innerHTML = carga 
+        let carga = `<div class="divsproducto">
+                <img src=${productoInfo.images[0]} alt="foto detalle de producto" class="fotoproducto">
+                <h3><a href="./category.html" class="category">Categoría: ${productoInfo.category} </a></h3>
+                <h2 class="precioprod">Precio: ${productoInfo.price}</h2>
+                <p class="tagsproducto">#${productoInfo.tags[0]} #${productoInfo.tags[1]}</p> 
+            </div>
+            <div class="divsproducto">
+                <div>
+                    <h1 class="nombreproducto">${productoInfo.title}</h1>
+                    <h2 class="titulodescripción">Descripción:</h2>
+                    <p class="descripción">${productoInfo.description}</p>
+                    <p class="stockproducto">Stock: ${productoInfo.stock}</p>
+                </div>
+            `
+        
+                for (let i=0; i<productoInfo.reviews.length; i++){
+                    carga += //Para q se agreguen a carga todos los reviews 
+                    `<div class="reviwsproducto">
+                        <h3 class="nombre reviews">Reviews de Usuarios:</h3>
+                        <p class="usuario nombre">${productoInfo.reviews[i].reviewerName}</p> 
+                        <p class="fecha">${productoInfo.reviews[i].date}</p>
+                        <p class="opinion">${productoInfo.reviews[i].comment}</p>
+                        <p class="rating">Rating: ${productoInfo.reviews[i].rating}</p>
+                     </div>
+                `}; //</div> queda abierto el div grande... esta mal en cuestión syntaxis perooooo anda REVISAR
+        
+        
+        producto.innerHTML = carga 
 
     })
     .catch(function (err) {
