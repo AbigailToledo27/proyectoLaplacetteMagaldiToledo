0d3423e7042503561bd839c984214a4e6f9181df
AbigailToledo27 <magtoledo@udesa.edu.ar>
2025-11-16 17:51:18 -0300

    termine categorias y lo vincule con el detalle del producto, tambien toque el css

---
+++ js/category.js
@@ -0,0 +1,39 @@
+let categoriaBuscada = location.search;
+let nombreCategoria = new URLSearchParams(categoriaBuscada);
+
+let categorySlug = nombreCategoria.get("category"); // Me debe devolver el nombre de la categoria del query, por ejemplo "smartphones"
+
+let tituloCategoria = document.querySelector(".name_category");
+tituloCategoria.innerText = categorySlug; //titulo de la categoria
+tituloCategoria.style.textTransform = "capitalize"; // Como use el Slug y no Name, del title, esta en minuscula, por eso lo modifique con style, para que la primera se mayus
+
+let url = `https://dummyjson.com/products/category/${categorySlug}`;
+
+fetch(url)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+   let productos = data.products ; // aca tomo el array del OBJ donde contiene la lista de propiedades de cada producto de las categorias
+   let seccionCategory = document.querySelector(".categorias");
+   let cardProductos = "";
+   for (let i = 0; i < productos.length; i++) {
+    cardProductos += `
+        <article>
+                <div>
+                    <img src="${productos[i].images[0]}" alt="${productos[i].title}">
+                    <h3>${productos[i].title}</h3>
+                    <p>${productos[i].description}</p>
+                </div>
+                <div>
+                    <p class="precio">Precio: ${productos[i].price}</p>
+                    <a href="./product.html?id=${productos[i].id}" class="boton-detalle">Ver detalle</a>
+                </div>
+        </article>
+    `}
+    seccionCategory.innerHTML = cardProductos;
+
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let url = "https://dummyjson.com/products/categories";
 
+// Barra de Busqueda
 let inputBusqueda = document.getElementById('searchInput');
 let mensajeError = document.querySelector('.error-message');
 let formHeader = document.querySelector('.barra-busqueda');
@@ -20,18 +21,19 @@ formHeader.addEventListener('submit', function(event){
     }
 });
 
+//Aside de categorias
 fetch(url)
 .then(function(response) {
   return response.json()
 })
 .then(function(data) {
-    let infoCategory = document.querySelector("#navCategorias"); //Data es un Array de Objetos de categoria con: "slug", "name" , "url"
+    let infoCategory = document.querySelector(".nav-categorias"); //Data es un Array de Objetos de categoria con: "slug", "name" , "url"
     let barraCategory = "";
     for (let i = 0; i < data.length; i++) {
         barraCategory += 
         `
-        <li class="category"><a href="./category.html?category=${data[i].name}">${data[i].name}</a></li>
-        `;
+        <li class="category"><a href="./category.html?category=${data[i].slug}">${data[i].name}</a></li>
+        `; //Uso "slug" en vez de "name" para el query porque en la API es el que va en la url de la categoria (empieza con minus) y en donde el usuario hace click si va name (empieza con mayus)
     }
     infoCategory.innerHTML = barraCategory; 
 })
