ec95a660e73b4952f35adabbbb7095a40dbf60a7
ylaplacette-code <ylaplacette@udesa.edu.ar>
2025-11-19 23:41:41 -0300

    estrucrura html logout

--- js/logout.js
+++ js/logout.js
@@ -3,14 +3,16 @@ window.addEventListener("load", function(){
 
     logoutBoton.addEventListener("click", function(){
         localStorage.clear();
-    })
-})
-
-mail = localStorage.getItem("email")
-
-if(mail == null){
+    
     let usu = document.querySelector(".usuarioSaludo")
     usu.style.display = "none"
     let usunav = document.querySelector(".navlogout")
     usunav.style.display = "none"
-}
+    let navheder = document.querySelector(".header-nav")
+    navheder.style.display = "flex"
+
+    })
+})
+
+///pasar header nav a display flex o blok
+
--- js/saludo.js
+++ js/saludo.js
@@ -1,15 +1,8 @@
 let header = document.querySelector(".header-nav")
-let links = document.querySelector(".nav-links")
 
 mail = localStorage.getItem("email")
 
-let formatoLogOut = `<h3 class = "usuarioSaludo">Bienvenido: ${mail}</h3>
-                    <nav class="header-nav navlogout">
-                        <ul class="nav-links">
-                            <li><a href="./index.html">Home</a></li>
-                            <li><a class= "botonLogout" >Logout</a></li>
-                        </ul>
-                    </nav>`; 
+let formatoLogOut = ``; 
 
 if(mail != ""){
     header.innerHTML = formatoLogOut
